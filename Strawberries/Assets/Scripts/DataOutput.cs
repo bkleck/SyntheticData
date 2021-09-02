d733eed315e93ec91f8c7c74bf1d453ded97c922
Boon Kong <kongleck@yahoo.com.sg>
2021-09-02 14:01:38 +0800

    configured data output

---
+++ Strawberries/Assets/Scripts/DataOutput.cs
@@ -0,0 +1,10 @@
+using Unity.Simulation;
+using UnityEngine;
+
+public class DataOutput : MonoBehaviour
+{
+    void Start()
+    {
+        Configuration.localPersistentDataPath = @"C:\Users\Boon Kong\Desktop\synthetic_data\labo";
+    }
+}
